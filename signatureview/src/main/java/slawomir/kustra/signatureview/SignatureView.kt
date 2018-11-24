a27d4a6daccebee84edadc3338be217a0af77031
skustra <kustraslawomir@gmail.com>
2018-11-24 21:14:43 +0100

    basic draw

--- signatureview/src/main/java/slawomir/kustra/signatureview/SignatureView.kt
+++ signatureview/src/main/java/slawomir/kustra/signatureview/SignatureView.kt
@@ -1,4 +1,49 @@
 package slawomir.kustra.signatureview
 
-public class SignatureView {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color.BLACK
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class SignatureView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val paint = Paint()
+    private val path = Path()
+
+    init {
+        setDefaultPaintStyle(paint)
+    }
+
+    private fun setDefaultPaintStyle(paint: Paint) {
+        with(paint) {
+            isAntiAlias = true
+            strokeWidth = 5f
+            color = BLACK
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPath(path, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> path.moveTo(x, y)
+            MotionEvent.ACTION_MOVE -> path.lineTo(x, y)
+        }
+
+        invalidate()
+        return true
+    }
 }
